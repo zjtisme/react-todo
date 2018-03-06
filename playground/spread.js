1f720ba4427d2f325696bf13be024142eeb1f24a
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-03-05 23:33:25 -0800

    Add playground

---
+++ playground/spread.js
@@ -0,0 +1,14 @@
+// function add(a, b) {
+//   return a + b;
+// }
+//
+// console.log(add(2, 2));
+//
+// var toAdd = [9, 5];
+// console.log(add(...toAdd));
+
+// var groupA=['Jen', 'Cory'];
+// var groupB=['Vikram'];
+//
+// var final=[3, ...groupA, ...groupB];
+// console.log(final);
