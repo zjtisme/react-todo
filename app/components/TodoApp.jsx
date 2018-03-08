4dc2ac4e6c8ba22ee9e150c997722c28fbf3e2e0
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-03-07 17:34:04 -0800

    refactor todosearch with redux store

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,31 +2,31 @@ var React=require('react');
 
 import TodoList from 'TodoList'
 import AddTodo from 'AddTodo'
-var TodoSearch=require('TodoSearch');
+import TodoSearch from 'TodoSearch'
 var uuid=require('node-uuid');
 var moment=require('moment');
 var TodoAPI=require('TodoAPI');
 
 var TodoApp=React.createClass({
-  getInitialState: function() {
-    return {
-      showCompleted: false,
-      searchText: '',
-      todos: TodoAPI.getTodos()
-    };
-  },
-  componentDidUpdate: function() {
-    TodoAPI.setTodos(this.state.todos);
-  },
-  handleSearch: function(showCompleted, searchText) {
-    this.setState({
-      showCompleted: showCompleted,
-      searchText: searchText.toLowerCase()
-    });
-  },
+  // getInitialState: function() {
+  //   return {
+  //     showCompleted: false,
+  //     searchText: '',
+  //     todos: TodoAPI.getTodos()
+  //   };
+  // },
+  // componentDidUpdate: function() {
+  //   TodoAPI.setTodos(this.state.todos);
+  // },
+  // handleSearch: function(showCompleted, searchText) {
+  //   this.setState({
+  //     showCompleted: showCompleted,
+  //     searchText: searchText.toLowerCase()
+  //   });
+  // },
   render: function() {
-    var {todos, showCompleted, searchText} = this.state;
-    var filterTodos=TodoAPI.filterTodos(todos, showCompleted, searchText);
+    // var {todos, showCompleted, searchText} = this.state;
+    // var filterTodos=TodoAPI.filterTodos(todos, showCompleted, searchText);
 
     return (
       <div>
@@ -35,7 +35,7 @@ var TodoApp=React.createClass({
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
-              <TodoSearch onSearch={this.handleSearch}/>
+              <TodoSearch/>
               <TodoList/>
               <AddTodo/>
             </div>
