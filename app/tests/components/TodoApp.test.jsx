8cef487430f3260d7c64a2bb72b65fd6659f3828
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-03-08 00:47:23 -0800

    Change all jQuery to jquery

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -2,7 +2,7 @@ var React=require('react');
 var ReactDOM=require('react-dom');
 var {Provider} = require('react-redux');
 var expect=require('expect');
-var $=require('jQuery');
+var $=require('jquery');
 var TestUtils=require('react-addons-test-utils');
 
 var configureStore = require('configureStore');
