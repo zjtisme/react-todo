0e5430b0a8e4658ebc5bc9c379a6769ca58810fb
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-03-07 23:23:47 -0800

    Update todos on firebase

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -4,20 +4,22 @@ var expect=require('expect');
 var $=require('jQuery');
 var TestUtils=require('react-addons-test-utils');
 
-var {Todo}=require('Todo');
+import * as actions from 'actions';
+import {Todo} from 'Todo';
 
 describe('Todo component', ()=>{
   it('should exist', ()=>{
     expect(Todo).toExist();
   });
 
-  it('should dispatch TOGGLE_TODO action on click', ()=>{
+  it('should dispatch startToggleTodo action on click', ()=>{
     var todoData={
       id: 199,
       text: 'Test',
       completed: false
     };
 
+    var action = actions.startToggleTodo(todoData.id, !todoData.completed);
     var spy=expect.createSpy();
     var todo=TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy}/>);
 
@@ -25,9 +27,6 @@ describe('Todo component', ()=>{
 
     TestUtils.Simulate.click($el[0]);
 
-    expect(spy).toHaveBeenCalledWith({
-      type: 'TOGGLE_TODO',
-      id: todoData.id
-    });
+    expect(spy).toHaveBeenCalledWith(action);
   });
 });
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -55,16 +55,21 @@ describe('Reducers', ()=>{
         createdAt: 123,
         completedAt: 126
       }];
-
+      var updates = {
+        completed: false,
+        completedAt: null
+      };
       var action = {
-        type: 'TOGGLE_TODO',
-        id: '123'
+        type: 'UPDATE_TODO',
+        id: todos[0].id,
+        updates
       };
 
       var res = reducers.todosReducer(df(todos), df(action));
 
-      expect(res[0].completed).toEqual(false);
-      expect(res[0].completedAt).toEqual(undefined);
+      expect(res[0].completed).toEqual(updates.completed);
+      expect(res[0].completedAt).toEqual(updates.completedAt);
+      expect(res[0].text).toEqual(todos[0].text);
     });
 
     it('should add existing todos', ()=> {
